1b6ff5127fdf29704b82e0d2b35961b45f8791ec
Pedro Pádua Miguel <pedropaduam.0702@gmail.com>
2026-08-12 11:30:14 -0300

    Tratamento dos anexos para df

---
+++ demonstrativo.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+class DemoTrib:
+    def __init__(self):
+        pass
+
+    def _get_anexos_data(self):
+        df_anexos = pd.read_excel('anexos.xls')
+        df_anexos = df_anexos.rename(columns={
+            'Anexo n .' : 'anexo',
+            'Nome do Anexo' : 'nm_anexo',
+            'Receita 12m\n(até) R$' : 'r12_max',
+            'Limite inferior\n(R$)': 'r12_min',
+            'Alíq. Nominal': 'aliq_nom',
+            'Valor a Deduzir\n(R$)': 'vlr_ded'
+        })
+        
+        df_anexos.columns = df_anexos.columns.str.lower()
